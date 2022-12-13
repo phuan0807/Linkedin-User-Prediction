e2ac6324ffda79b56a12bcb81d037ca500ba4faf
phuan0807 <120279571+phuan0807@users.noreply.github.com>
2022-12-13 16:44:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import pandas as pd
 import numpy as np
 import altair as alt
 #import sklearn as sk
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import classification_report
+#from sklearn.metrics import confusion_matrix
 #### Add header to describe app
 st.markdown("# Are you a Linkedin User?")
 st.markdown("By Patrick Huang")
